6ff9e54a9674d02913f5067f8ed7511ef31a02ad
Andreas Kurz <off.andreaskurz+github@gmail.com>
2025-10-17 19:59:35 +0200

    updated dotfiles with install script because stow does not always have dotfiles option

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+sudo apt install fzf
+
+# zoxide
+if ! hash zoxide 2>/dev/null; then
+    curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+fi
+
+if [ -e ~/.bashrc ] && [ ! -L ~/.bashrc ]; then
+    echo "Moving current .bashrc to .bashrc.bkp"
+    mv ~/.bashrc ~/.bashrc.bkp
+fi
+ln -sf ~/dotfiles/dot-bashrc ~/.bashrc
+ln -sf ~/dotfiles/dot-gitconfig ~/.gitconfig 
+ln -sf ~/dotfiles/dot-fonts ~/.fonts
+
+ln -sf ~/dotfiles/dot-config/fontconfig ~/.config/
+ln -sf ~/dotfiles/dot-config/ghostty ~/.config/
+
+source ~/.bashrc
