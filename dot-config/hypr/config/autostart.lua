6d0f10dbead03601807b138632bbfee8f219d35e
Andreas Kurz <off.andreaskurz+github@gmail.com>
2026-08-25 11:32:42 +0200

    use the nm-applet for the password input for vpn

--- dot-config/hypr/config/autostart.lua
+++ dot-config/hypr/config/autostart.lua
@@ -4,5 +4,6 @@
 hl.on("hyprland.start", function ()
     hl.exec_cmd("dbus-update-activation-environment --systemd --all")
     hl.exec_cmd("noctalia")
+    hl.exec_cmd("nm-applet")
     hl.exec_cmd("xhost +SI:localuser:root")
 end)
